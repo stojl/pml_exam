ba1ccdcb9fca480f8fe5f370357d73432a08b3c8
stojl <lucashillerod@hotmail.com>
2023-01-10 15:16:03 +0100

    vae

---
+++ LinearEncoderDecoder.py
@@ -0,0 +1,46 @@
+from torch import nn
+import torch
+
+class LinearDecoder(nn.Module):
+        def __init__(self, z_dim, h1_dim, h2_dim):
+            super().__init__()
+            
+            self.z_dim = z_dim
+            
+            self.fc1 = nn.Linear(z_dim, h2_dim)
+            self.fc1a = nn.Linear(h2_dim, h1_dim)
+            self.fc2 = nn.Linear(h1_dim, 784)
+               
+            self.softplus = nn.Softplus()
+            self.sigmoid = nn.Sigmoid()
+            
+        def forward(self, z):
+            h1 = self.softplus(self.fc1(z))
+            h1a = self.softplus(self.fc1a(h1))
+            
+            img = self.sigmoid(self.fc2(h1a))
+            return img
+
+class LinearEncoder(nn.Module):
+        def __init__(self, z_dim, h1_dim, h2_dim):
+            super().__init__()
+            
+            self.z_dim = z_dim
+            
+            self.fc1 = nn.Linear(784, h1_dim)
+            self.fc1a = nn.Linear(h1_dim, h2_dim)
+            self.fc21 = nn.Linear(h2_dim, self.z_dim)
+            self.fc22 = nn.Linear(h2_dim, self.z_dim)
+            
+            self.softplus = nn.Softplus()
+            
+        def forward(self, x):
+            x = x.reshape(-1, 784)
+            
+            h1 = self.softplus(self.fc1(x))
+            h1a = self.softplus(self.fc1a(h1))
+            
+            z_mean = self.fc21(h1a)
+            z_var = torch.exp(self.fc22(h1a))
+            
+            return z_mean, z_var
