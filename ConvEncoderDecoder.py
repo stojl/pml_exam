48d59217dc10233b5a248a7912bab65a165e7472
stojl <lucashillerod@hotmail.com>
2023-01-12 11:01:53 +0100

    hello

--- ConvEncoderDecoder.py
+++ ConvEncoderDecoder.py
@@ -7,10 +7,9 @@ class ConvDecoder(nn.Module):
         self.z_dim = z_dim 
         
         # setup the two linear transformations used
-        self.fc1 = nn.Linear(z_dim, 3136)
-        self.decConv1 = nn.ConvTranspose2d(64, 64, kernel_size=4, padding = 1, stride = 2) # OUTPUT SHAPE (64, 14, 14)
-        self.decConv2 = nn.ConvTranspose2d(64, 32, kernel_size=4, padding = 1, stride = 2) # OUTPUT SHAPE (32, 28, 28)
-        self.decConv3 = nn.ConvTranspose2d(32, 1, kernel_size=3, padding = 1) # OUTPUT SHAPE (1, 28, 28)
+        self.fc1 = nn.Linear(z_dim, hidden_dim)
+        self.fc2 = nn.Linear(hidden_dim, 32 * 14 * 14)
+        self.decConv1 = nn.ConvTranspose2d(32, 1, kernel_size=4, padding=1, stride=2)
         
         # setup the non-linearities
         self.relu = nn.ReLU()
@@ -18,10 +17,9 @@ class ConvDecoder(nn.Module):
 
     def forward(self, z):
         z = self.relu(self.fc1(z))
-        z = z.reshape(-1, 64, 7, 7)
-        z = self.relu(self.decConv1(z))
-        z = self.relu(self.decConv2(z))
-        z = self.sigmoid(self.decConv3(z))
+        z = self.relu(self.fc2(z))
+        z = z.reshape(-1, 32, 14, 14)
+        z = self.sigmoid(self.decConv1(z))
         return z.reshape(-1, 784)
 
 class ConvEncoder(nn.Module):
@@ -31,8 +29,7 @@ class ConvEncoder(nn.Module):
         
         # setup the three linear transformations used
         self.encConv1 = nn.Conv2d(1, 32, kernel_size = 3, padding = 1, stride = 2) # OUTPUT SHAPE (32, 14, 14)
-        self.encCon2 = nn.Conv2d(32, 64, kernel_size = 3, padding = 1, stride = 2) # OUTPUT SHAPE (64, 7, 7)
-        self.f1a = nn.Linear(64 * 7 * 7, hidden_dim)
+        self.f1a = nn.Linear(32 * 14 * 14, hidden_dim)
         self.fc21 = nn.Linear(hidden_dim, z_dim)
         self.fc22 = nn.Linear(hidden_dim, z_dim)
         # setup the non-linearities
@@ -41,7 +38,6 @@ class ConvEncoder(nn.Module):
     def forward(self, x):
         x = x.reshape(-1, 1, 28, 28)
         x = self.relu(self.encConv1(x))
-        x = self.relu(self.encCon2(x))
         x = torch.flatten(x, 1)
         x = self.relu(self.f1a(x))
         z_loc = self.fc21(x)
