ed57a4658c25727d90be9719ade7db6f3e4c218b
stojl <lucashillerod@hotmail.com>
2023-01-16 14:40:43 +0100

    save

--- diffusion.py
+++ diffusion.py
@@ -56,7 +56,7 @@ class DiffusionModel(nn.Module):
         return alpha
     
     def embedtime(self, t, embedding_size, dimensions):
-        w = 1.0 / (10000 ** (torch.arange(0, embedding_size, 2, dtype=torch.float32) / embedding_size))
+        w = 1.0 / (10000 ** (torch.arange(0, embedding_size, 2, dtype=torch.float32, device=self.device) / embedding_size))
         y = torch.sin(t.view(t.shape[0], 1).repeat(1, embedding_size // 2) * w)
         x = torch.cos(t.view(t.shape[0], 1).repeat(1, embedding_size // 2) * w)
         return torch.cat([x, y], dim=1).view(-1, embedding_size, 1, 1).repeat(1, 1, dimensions, dimensions)
