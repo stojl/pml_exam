3cab1f48fcf12cd2569f03968a8dc89fe473c54d
stojl <lucashillerod@hotmail.com>
2023-01-17 15:08:45 +0100

    save

--- VAEBayesian.py
+++ VAEBayesian.py
@@ -59,14 +59,14 @@ class BayesianVAE(nn.Module):
         b1 = pyro.sample("b1", pdist.Normal(b1_mean, b1_sd).expand([self.h2_dim]).to_event(1))
         
         w2_sd = pyro.param("w2_sd", torch.tensor(1.0, device=self.device), constraint=constraints.positive)
-        w2_mean = pyro.param("w1_mean", torch.tensor(1.0, device=self.device))
+        w2_mean = pyro.param("w2_mean", torch.tensor(1.0, device=self.device))
         b2_sd = pyro.param("b2_sd", torch.tensor(1.0, device=self.device), constraint=constraints.positive)
         b2_mean = pyro.param("b2_mean", torch.tensor(1.0, device=self.device))
         w2 = pyro.sample("w2", pdist.Normal(w2_mean, w2_sd).expand([self.h2_dim, self.h1_dim]).to_event(2))
         b2 = pyro.sample("b2", pdist.Normal(b2_mean, b2_sd).expand([self.h1_dim]).to_event(1))
         
         w3_sd = pyro.param("w3_sd", torch.tensor(1.0, device=self.device), constraint=constraints.positive)
-        w3_mean = pyro.param("w1_mean", torch.tensor(1.0, device=self.device))
+        w3_mean = pyro.param("w3_mean", torch.tensor(1.0, device=self.device))
         b3_sd = pyro.param("b3_sd", torch.tensor(1.0, device=self.device), constraint=constraints.positive)
         b3_mean = pyro.param("b3_mean", torch.tensor(1.0, device=self.device))
         w3 = pyro.sample("w3", pdist.Normal(w3_mean, w3_sd).expand([self.h1_dim, 784]).to_event(2))
