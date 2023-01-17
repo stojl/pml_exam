2eca2c625e9c7feadb6f748fdb74116cee7298f5
stojl <lucashillerod@hotmail.com>
2023-01-17 14:01:50 +0100

    save

--- VAEBayesian.py
+++ VAEBayesian.py
@@ -29,80 +29,47 @@ class BayesianVAE(nn.Module):
             self.cuda()
             
     def model(self, x):
-        print(x.device)
-        
         w1 = pyro.sample("w1", pdist.Normal(0, self.sd).expand([self.z_dim, self.h2_dim]).to_event(2))
-        print(w1.device)
         b1 = pyro.sample("b1", pdist.Normal(0, self.sd).expand([self.h2_dim]).to_event(1))
-        print(b1.device)
         
         w2 = pyro.sample("w2", pdist.Normal(0, self.sd).expand([self.h2_dim, self.h1_dim]).to_event(2))
-        print(w2.device)
         b2 = pyro.sample("b2", pdist.Normal(0, self.sd).expand([self.h1_dim]).to_event(1))
-        print(b2.device)
         
         w3 = pyro.sample("w3", pdist.Normal(0, self.sd).expand([self.h1_dim, 784]).to_event(2))
-        print(w3.device)
         b3 = pyro.sample("b3", pdist.Normal(0, self.sd).expand([784]).to_event(1))
-        print(b3.device)
 
         with pyro.plate("data", x.shape[0]):
             z_mean = torch.zeros(x.shape[0], self.z_dim, dtype=x.dtype, device=self.device)
-            print(z_mean.device)
             z_var = torch.ones(x.shape[0], self.z_dim, dtype=x.dtype, device=self.device)
-            print(z_var.device)
             
             z = pyro.sample("z", pdist.Normal(z_mean, z_var).to_event(1))
-            print(z.device)
             h1 = self.relu(z @ w1 + b1)
-            print(h1.device)
             h2 = self.relu(h1 @ w2 + b2)
-            print(h2.device)
             img = self.sigmoid(h2 @ w3 + b3)
-            print(img.device)
-            t = pyro.sample("obs", pdist.Bernoulli(img, validate_args = False).to_event(1), obs=x.reshape(-1, 784))
-            print(t.device)
-            return t
+            pyro.sample("obs", pdist.Bernoulli(img, validate_args = False).to_event(1), obs=x.reshape(-1, 784))
             
     def guide(self, x):
         pyro.module("encoder", self.encoder)
-        print(x.device)
         
         w1_map = pyro.param("w1_map", torch.tensor(0.0, device=self.device))
-        print(w1_map.device)
         b1_map = pyro.param("b1_map", torch.tensor(1.5, device=self.device))
-        print(b1_map.device)
         w1 = pyro.sample("w1", pdist.Delta(w1_map).expand([self.z_dim, self.h2_dim]).to_event(2))
-        print(w1.device)
         b1 = pyro.sample("b1", pdist.Delta(b1_map).expand([self.h2_dim]).to_event(1))
-        print(b1.device)
         
         w2_map = pyro.param("w2_map", torch.tensor(0.0, device=self.device))
-        print(w2_map.device)
         b2_map = pyro.param("b2_map", torch.tensor(0.5, device=self.device))
-        print(b2_map.device)
         w2 = pyro.sample("w2", pdist.Delta(w2_map).expand([self.h2_dim, self.h1_dim]).to_event(2))
-        print(w2.device)
         b2 = pyro.sample("b2", pdist.Delta(b2_map).expand([self.h1_dim]).to_event(1))
-        print(b2.device)
         
         w3_map = pyro.param("w3_map", torch.tensor(0.0, device=self.device))
-        print(w3_map.device)
         b3_map = pyro.param("b3_map", torch.tensor(2.0, device=self.device))
-        print(b3_map.device)
         w3 = pyro.sample("w3", pdist.Delta(w3_map).expand([self.h1_dim, 784]).to_event(2))
-        print(w3.device)
         b3 = pyro.sample("b3", pdist.Delta(b3_map).expand([784]).to_event(1))
-        print(b3.device)
         
         with pyro.plate("data", x.shape[0]):
             z_mean, z_var = self.encoder.forward(x)
-            print(z_mean.device)
-            print(z_var.device)
             # Reparametrization trick in disguise
-            t = pyro.sample("z", pdist.Normal(z_mean, z_var).to_event(1))
-            print(t.device)
-            return t
+            pyro.sample("z", pdist.Normal(z_mean, z_var).to_event(1))
             
     def reconstruct_img(self, x):
         z_mean, z_var = self.encoder(x)
